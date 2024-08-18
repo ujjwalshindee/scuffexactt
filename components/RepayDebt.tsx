95ed4ba679f6f05e67d96978f264b34feba16041
ujjwalshindee <ujjwalshindee@gmail.com>
2024-08-18 21:32:59 +0530

    implement appwrite

--- components/RepayDebt.tsx
+++ components/RepayDebt.tsx
@@ -17,50 +17,50 @@ import {
   TabsTrigger,
 } from "@/components/ui/tabs"
 import {
-    AlertDialog,
-    AlertDialogAction,
-    AlertDialogCancel,
-    AlertDialogContent,
-    AlertDialogDescription,
-    AlertDialogFooter,
-    AlertDialogHeader,
-    AlertDialogTitle,
-    AlertDialogTrigger,
-  } from "@/components/ui/alert-dialog"
-  
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+
 
 const RepayDebt = () => {
   return (
     <Tabs defaultValue="extend-debt" className="">
-    <TabsList className="grid w-full grid-cols-2">
-      <TabsTrigger value="extend-debt">Extend Debt</TabsTrigger>
-      <TabsTrigger value="clear-debt">Clear Debt</TabsTrigger>
-    </TabsList>
-    <TabsContent value="extend-debt">
-      <Card>
-        <CardHeader>
-          <CardDescription>
-          Please note that a 10% fee applies for extending this debt by 30 days. The extension will be processed once your payment is confirmed.
-          </CardDescription>
-        </CardHeader>
-        <CardFooter>
-        <Button variant={'outline'}>Proceed to Pay</Button>
-        </CardFooter>
-      </Card>
-    </TabsContent>
-    <TabsContent value="clear-debt">
-      <Card>
-        <CardHeader>
-          <CardDescription>
-          Your debt will be finalized once we confirm your payment.
-          </CardDescription>
-        </CardHeader>
-        <CardFooter>
-          <Button variant={'outline'}>Proceed to Pay</Button>
-        </CardFooter>
-      </Card>
-    </TabsContent>
-  </Tabs>
+      <TabsList className="grid w-full grid-cols-2">
+        <TabsTrigger value="extend-debt">Extend Debt</TabsTrigger>
+        <TabsTrigger value="clear-debt">Clear Debt</TabsTrigger>
+      </TabsList>
+      <TabsContent value="extend-debt">
+        <Card>
+          <CardHeader>
+            <CardDescription>
+              Please note that a 10% fee applies for extending this debt by 30 days. The extension will be processed once your payment is confirmed.
+            </CardDescription>
+          </CardHeader>
+          <CardFooter>
+            <Button variant={'outline'}>Proceed to Pay</Button>
+          </CardFooter>
+        </Card>
+      </TabsContent>
+      <TabsContent value="clear-debt">
+        <Card>
+          <CardHeader>
+            <CardDescription>
+              Your debt will be finalized once we confirm your payment.
+            </CardDescription>
+          </CardHeader>
+          <CardFooter>
+            <Button variant={'outline'}>Proceed to Pay</Button>
+          </CardFooter>
+        </Card>
+      </TabsContent>
+    </Tabs>
   )
 }
 
